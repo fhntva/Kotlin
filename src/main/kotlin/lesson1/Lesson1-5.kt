1efb970b8b18903a066aed90993a5eb3607f7b45
fhntva <fhntv.apt@yandex.ru>
2024-09-12 17:35:19 +0300

    lesson 1-5 clean

--- src/main/kotlin/lesson1/Lesson1-5.kt
+++ src/main/kotlin/lesson1/Lesson1-5.kt
@@ -5,23 +5,23 @@ fun main()
 // 108 минуты // время полета Гагарина || 6480
     var starter= 6480
 
-    var (second, minutes,hours ) = Triple(60, 60, 3600)
+    val (minutes,hours ) = listOf( 60, 3600)
 
 
 
-    var hoursfly:Int =  (starter/hours)
-    starter = starter-(hoursfly*hours)
+    val hoursfly:Int =  (starter/hours)
+    starter -= (hoursfly * hours)
 
 
 
-    var minutefly:Int = (starter/minutes)
-    starter = starter-(minutefly*minutes)
+    val minutefly:Int = (starter/minutes)
+    starter -= (minutefly * minutes)
 
 
-    var secondfly:Int = starter
+    val secondfly:Int = starter
 
 
-    println("Время проведенное Гагариным в космосе равно: "+hoursfly+" час "+ minutefly+" минут "+ secondfly+" секунд")
+    println("Время проведенное Гагариным в космосе равно: $hoursfly час $minutefly минут $secondfly секунд")
 
 
 }
