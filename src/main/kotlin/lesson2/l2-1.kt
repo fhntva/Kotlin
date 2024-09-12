289c6b337c439c6046fb2ca4ef4a644758ce9aec
fhntva <fhntv.apt@yandex.ru>
2024-09-12 19:13:22 +0300

    l2-1 arithmetic mean of array

---
+++ src/main/kotlin/lesson2/l2-1.kt
@@ -0,0 +1,25 @@
+package lesson2
+
+fun main ()
+{
+    val numbers = arrayOf(3,4,3,5)
+    var sum = 0.00
+    var calc = 0
+
+    for (i in 0..numbers[calc])
+    {
+
+
+        sum += numbers[calc]
+
+    //println(sum)
+
+        calc++
+
+    }
+
+    val average : Double  = sum/calc
+
+    println(average)
+
+}
