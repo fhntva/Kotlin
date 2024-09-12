bf9c4ed91ea2c9097f5835f9e1e639f0c3e56d6a
fhntva <fhntv.apt@yandex.ru>
2024-09-12 17:07:24 +0300

    lesson 1-4

--- src/main/kotlin/lesson1/Lesson1-3.kt
+++ src/main/kotlin/lesson1/Lesson1-3.kt
@@ -1,3 +1,4 @@
+package lesson1
 fun main () {
 // Задача о Гагарине.
     val years= "1961"; var ( hours,minute ) = listOf("09 ","07 ")
---
+++ src/main/kotlin/lesson1/Lesson1-4.kt
@@ -0,0 +1,20 @@
+package lesson1
+fun main()
+{
+    val lenght:Long = 40868600000
+
+    val age:Byte= 27
+
+    val partofday:Float = 0.075F
+    val sec:Short = 6480
+    val partofyear:Float = 2.0547945205479453E-4F
+    val apogee:Int = 327000;
+
+
+
+
+
+    println(lenght)
+    println (""+age+"\n"+partofday+"\n"+partofyear+"\n"+ apogee)
+    // ахуенно нихуя не складывается
+}
