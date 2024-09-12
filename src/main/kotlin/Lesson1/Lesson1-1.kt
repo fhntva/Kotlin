b6adb34b2f40aa7b76d4adb569fa2bca27d4ac1f
fhntva <fhntv.apt@yandex.ru>
2024-09-12 15:41:12 +0300

    lesson 1-1 confirm

---
+++ src/main/kotlin/Lesson1/Lesson1-1.kt
@@ -0,0 +1,13 @@
+package Lesson1
+
+fun main () {
+    // Количество ордеров
+    var quantityOfOrder = 75;
+
+    val purchase: String = "Thank you for purchase " ;
+    val order: String = " order";
+
+
+    println(purchase+quantityOfOrder+order);
+
+}
