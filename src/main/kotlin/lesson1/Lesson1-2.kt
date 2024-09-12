f380ab3bd85b26cbe7fc27f6e68bd2c18492fd99
fhntva <fhntv.apt@yandex.ru>
2024-09-12 16:33:42 +0300

    lesson 1-2 cleanup

---
+++ src/main/kotlin/lesson1/Lesson1-2.kt
@@ -0,0 +1,22 @@
+package lesson1
+
+fun main () {
+// задаем зачем то количесво работающего люда
+    val quantityOfOrder = 75
+
+    val purchase = "Thank you for purchase "
+    val order = " order"
+
+
+    println(purchase+quantityOfOrder+order)
+
+
+    var workers = 2000
+
+    workers -=1
+
+    println("Количество работников = $workers")
+
+    // коммент для комента
+
+}
