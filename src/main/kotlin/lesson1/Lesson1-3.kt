46a0edfa7c4f9c89729d55c4c60dbb9f278575d7
fhntva <fhntv.apt@yandex.ru>
2024-09-12 16:49:06 +0300

    lesson 1-3

---
+++ src/main/kotlin/lesson1/Lesson1-3.kt
@@ -0,0 +1,10 @@
+fun main () {
+// Задача о Гагарине.
+    val years= "1961"; var ( hours,minute ) = listOf("09 ","07 ")
+
+    println("Время полета : Год $years\nЧасы $hours\nминуты $minute")
+    println("")
+    hours="10";minute="55"
+    println("Время посадки : Год $years\nЧасы $hours\nминуты $minute")
+
+}
