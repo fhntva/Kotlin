3f35d1aa780b0d6a2a433ee8d694aa408f1619c1
fhntva <fhntv.apt@yandex.ru>
2024-09-14 20:11:17 +0300

    L3-4 chess move counter

---
+++ src/main/kotlin/lesson3/L3-4.kt
@@ -0,0 +1,24 @@
+package lesson3
+
+// счетчик ходов в шахматах Ход из E2 В E4 номер хода 1
+//                          Ход из E2 В E4 номер хода 2
+
+fun main ()
+{
+            val corA: String = "E2"
+            val corB: String = "E4"
+            val step: String = "1"
+
+        println( "Ход из "+ corA + " В " + corB +" номер хода "+ step  )
+
+        val corA1: String = "E2"
+        val corB1: String = "E4"
+        val step1: String = "2"
+
+        println( "Ход из "+ corA1 + " В " + corB1 +" номер хода "+ step1   )
+
+}
+
+
+
+
