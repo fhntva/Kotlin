70df21ddece22d20801de2e52ae8f4f54b782628
fhntva <fhntv.apt@yandex.ru>
2024-09-24 01:59:56 +0300

    L7-4 cycle FOR time

---
+++ src/main/kotlin/lesson7/L7-4.kt
@@ -0,0 +1,27 @@
+package lesson7
+
+fun main ()
+{
+    println("Введите количество секунд для секундомер")
+    var second = readln().toInt()
+
+    var a = 1
+
+    for ( i in a..second )
+    {
+
+
+
+        var howMuchTime = second-i
+
+
+        println("Прошло секунд : $i Осталось времени $howMuchTime")
+        if (howMuchTime==0) {break}
+        Thread.sleep(1000)
+
+
+
+
+    }
+
+}
