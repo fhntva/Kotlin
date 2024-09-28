e5e4b8c052bd87171493001db62a20507fcad8c6
fhntva <fhntv.apt@yandex.ru>
2024-09-29 00:08:58 +0300

    L9-5 1 element changer in list

--- src/main/kotlin/lesson9/L9-5.kt
+++ src/main/kotlin/lesson9/L9-5.kt
@@ -4,7 +4,12 @@ fun main() {
 
 
     println("Введите 5 элементов друг за другом")
-    var setMe = mutableListOf(readln(), readln(), readln(), readln(), readln())
-    println(setMe.toSet().sorted().map { it.capitalize() }) // удаляет дубликаты set // сортирует по порядку // заглавные буквы
+    val setMe = mutableListOf(readln(), readln(), readln(), readln(), readln())
+    var setMeCopy = setMe.toSet().sorted().toMutableList() // удаляет дубликаты set // сортирует по порядку // заглавные буквы .map { it.capitalize() }
+    var int = setMeCopy.get(0).toString().replaceFirstChar { it.uppercaseChar() }
+    setMeCopy[0] = int
+
+    println(setMe) //"первый массив"
+    println(setMeCopy) // "измененный массив"
 
 }
