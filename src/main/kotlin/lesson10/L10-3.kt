2ed34281bf29a65fb9a218a54c0f04dccc8a168f
fhntva <fhntv.apt@yandex.ru>
2024-09-30 23:33:59 +0300

    L10-3 shitiest passwordgen

---
+++ src/main/kotlin/lesson10/L10-3.kt
@@ -0,0 +1,63 @@
+package lesson10
+
+fun main() {
+    println("Добро пожаловать в генерацию плохого пароля")
+    println("Введите длинну пароля ")
+    val simeMe = readln().toInt()
+    var passwordGen = passwordgen(simeMe).toString()
+    println("Ващ пароль $passwordGen ")
+
+
+}
+
+
+fun randMeFullPack(): String {
+    val diap = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+    val randMe = diap.random().toString()
+    return randMe
+
+}
+
+fun randMeSpecials(): String {
+    val diap = listOf('!', '@', '#', '$', '%', '^', '&', '*', '(', ')') // тут всю чушню перечислить
+    val passMe = diap.random()
+    return passMe.toString()
+}
+
+fun randmeNumber(): String {
+    val diap = 1..9
+    val randNumber = diap.random()
+
+    return randNumber.toString()
+}
+
+fun probel(): String {
+    val probel = " ".toString()
+    return probel
+}
+
+fun passwordgen(Size: Int): String {
+    val quant = Size
+    var password: String = randmeNumber().toString()
+    password += randMeSpecials().toString()
+    password += probel()
+    val diap = 1..2
+
+
+    for (i in 1..quant - 3) {
+        var oneORtwo = diap.random()
+        if (1 == oneORtwo) {
+            password += randmeNumber()
+
+
+        }
+        if (2 == oneORtwo) {
+            password += randMeSpecials()
+
+        }
+
+    }
+
+    return password
+
+}
