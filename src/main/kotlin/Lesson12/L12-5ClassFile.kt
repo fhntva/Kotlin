bc61565737b7372d121433301ef82f81538cafef
fhntva <fhntv.apt@yandex.ru>
2024-10-09 17:50:04 +0300

    L12-5 средняя температура и осадки

---
+++ src/main/kotlin/Lesson12/L12-5ClassFile.kt
@@ -0,0 +1,11 @@
+package Lesson12
+
+class averageWeather(day:MutableList<Int> ,night: MutableList<Int> , fallout:MutableList<String>)
+{
+    var day = day
+    var night = night
+    var fallout = fallout
+
+
+
+}
