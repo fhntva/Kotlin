b1f168e6e3a7d2649c3ce026bccd6fc5eea34d35
fhntva <fhntv.apt@yandex.ru>
2024-10-09 17:03:42 +0300

    L12-2 конвертер кельвинов в цельсий

---
+++ src/main/kotlin/Lesson12/L12-3.kt
@@ -0,0 +1,10 @@
+package Lesson12
+import WeatherConverter
+
+
+fun main()
+{
+    var someS = WeatherConverter(  // передаем температуру в кельвинах, получаем в цельсиях
+        first = 3005,
+        second = 280,)
+}
