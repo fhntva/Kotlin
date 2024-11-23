7df9783117b3d17e9acef5e8707178cd6513a074
fhntva <fhntv.apt@yandex.ru>
2024-11-23 23:52:36 +0300

    L17-2 rewriter class name

---
+++ src/main/kotlin/Lesson17/L17-2Class.kt
@@ -0,0 +1,17 @@
+package Lesson17
+
+class Ship() {
+
+    var name = "Sadness"
+        set(value) {
+
+            println("Not correct name")
+        }
+    var speed = 150
+    var port = "trip"
+}
+
+
+
+
+
---
+++ src/main/kotlin/Lesson17/L17-2Main.kt
@@ -0,0 +1,10 @@
+package Lesson17
+
+fun main()
+{
+    var ship = Ship()
+
+    println(ship.name)
+    ship.name = "sad"
+    println(ship.name)
+}
