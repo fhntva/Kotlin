b56d8e7a8767e6aad3b2187b3c95aaa5883d8fc8
fhntva <fhntv.apt@yandex.ru>
2024-11-26 00:59:28 +0300

    L18-1

---
+++ src/main/kotlin/Lesson18/L18-1Main.kt
@@ -0,0 +1,10 @@
+package Lesson18
+
+fun main() {
+        var package1 = Order(2, "сумка")
+    var package2 = Order(3,  mutableListOf("сумка", "хуюмка"))
+    package1.order(package1.packages)
+    package2.order(package2.Morepackages)
+
+}
+
