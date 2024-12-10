b3227d5ffc26f42af331178ca61c4bfae30355a9
fhntva <fhntv.apt@yandex.ru>
2024-12-10 11:16:46 +0300

    L20-3 OpenDoor KeyChecker

---
+++ src/main/kotlin/Lesson20/L20-3Main.kt
@@ -0,0 +1,12 @@
+package Lesson20
+
+fun main() {
+    var oleg = gamer()
+    run {
+        if (oleg.key) {
+            println("Олег открыл дверь")
+        } else {
+            println("ключика нет")
+        }
+    }
+}
