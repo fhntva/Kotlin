95b105be95b8bdb6c0577b3bb49658f5ceeac9c0
fhntva <fhntv.apt@yandex.ru>
2024-12-14 17:44:10 +0300

    L21-2 filteredSum

--- src/main/kotlin/Lesson21/L21-2Main.kt
+++ src/main/kotlin/Lesson21/L21-2Main.kt
@@ -5,7 +5,6 @@ import L1_10.lesson1.lesson10.randmeNumber
 fun main() {
     var numberList = listOf(3, 5, 7, 123, 5435,  123, 15, 127, 13, 99, 4, 6, 8) // 18 of all
     println(numberList.evenNumbersSum())
-
 }
 
 
